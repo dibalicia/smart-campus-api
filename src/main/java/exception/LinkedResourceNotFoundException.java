03e6b5920d8d2d8eb18e849fbf70fa3894c4f8c0
Dib Alicia <aliciadib.cs@gmail.com>
2026-04-24 05:18:07 +0100

    LinkedResourceNotFoundException.java

--- src/main/java/exception/LinkedResourceNotFoundException.java
+++ src/main/java/exception/LinkedResourceNotFoundException.java
@@ -1,10 +1,6 @@
 package exception;
 
-/**
- * Thrown when a resource references another resource (e.g., a roomId inside a
- * sensor POST body) that does not exist in the system.
- * Mapped to HTTP 422 Unprocessable Entity by LinkedResourceNotFoundExceptionMapper.
- */
+
 public class LinkedResourceNotFoundException extends RuntimeException {
 
     public LinkedResourceNotFoundException(String message) {
